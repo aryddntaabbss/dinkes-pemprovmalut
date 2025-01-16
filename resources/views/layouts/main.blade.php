62409f6c273eeed478cb7f6603c5b397a2b62d16
aryaddinata <aryaddinataabbas017@gmail.com>
2025-01-16 19:05:22 +0900

    add main

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Favicons -->
+        <link href="{{ asset('images/logo.png') }}" rel="icon">
+        <link href="{{ asset('images/logo.png') }}" rel="apple-touch-icon">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('components.top-bar')
+            @include('components.header')
+
+            <!-- Page Content -->
+            <main class="flex-grow">
+                @yield('body')
+            </main>
+            @include('components.footer')
+        </div>
+    </body>
+
+</html>
