c9fcf4edb940ec9ceb7bbcfb0a23d0e741387b25
aryaddinata <aryaddinataabbas017@gmail.com>
2025-01-16 19:05:54 +0900

    add top-bar

---
+++ resources/views/components/top-bar.blade.php
@@ -0,0 +1,25 @@
+<!-- Top Bar -->
+<div class="bg-teal-800 text-white py-2">
+    <div class="container mx-auto flex justify-between items-center px-4">
+        <!-- Left Menu -->
+        <div class="flex space-x-4">
+            <a href="#" class="text-sm hover:underline">Peta Situs</a>
+            <a href="#" class="text-sm hover:underline">FAQ</a>
+        </div>
+        <!-- Right Social Media -->
+        <div class="flex space-x-4">
+            <a href="#" class="hover:text-gray-300">
+                <i class="fab fa-facebook"></i> <!-- Facebook -->
+            </a>
+            <a href="#" class="hover:text-gray-300">
+                <i class="fab fa-twitter"></i> <!-- Twitter -->
+            </a>
+            <a href="#" class="hover:text-gray-300">
+                <i class="fab fa-instagram"></i> <!-- Instagram -->
+            </a>
+            <a href="#" class="hover:text-gray-300">
+                <i class="fab fa-youtube"></i> <!-- YouTube -->
+            </a>
+        </div>
+    </div>
+</div>
